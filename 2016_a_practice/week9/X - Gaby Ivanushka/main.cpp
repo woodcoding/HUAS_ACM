915f3755e911737ec4a6eb9cbe083570fc82d0e4
woodcoding <woodcoding@gmail.com>
2016-12-05 19:26:35 +0800

    看代码证明的问题

---
+++ 2016_a_practice/week9/X - Gaby Ivanushka/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", i);
+    }
+    putchar('\n');
+    return 0;
+}
