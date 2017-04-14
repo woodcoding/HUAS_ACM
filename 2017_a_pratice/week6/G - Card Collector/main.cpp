2bb7f3ddd4e8ca0cf9238a683d1065727187bb91
woodcoding <woodcoding@gmail.com>
2017-04-14 16:10:13 +0800

    修改了一下python的代码以免TLE，做了一个状态压缩题

---
+++ 2017_a_pratice/week6/G - Card Collector/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <memory.h>
+using namespace std;
+
+const int maxn=22;
+int n, inf;
+double p[maxn], dp[1<<maxn];
+double nop;
+
+int main()
+{
+    while(~scanf("%d", &n)){
+        inf=1<<n;
+        nop = 1;
+        for(int i=0;i<n;i++){
+            scanf("%lf", &p[i]);
+            nop-=p[i];
+        }
+        dp[inf-1]=0;
+        for(int i=inf-2;i>=0;i--){
+            dp[i]=1;
+            double g=nop;
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))==0){    //如果没有当前的卡片则加入拿到这张卡后的期望
+                    dp[i]+=p[j]*dp[i|(1<<j)];
+                }
+                else{               //如果当前卡片已经存在或者没卡
+                    g+=p[j];
+                }
+            }
+            dp[i]/=(1-g);
+        }
+        printf("%.3lf\n", dp[0]);
+    }
+    return 0;
+}
--- 2017_a_pratice/week6/H - Patchouli Spell Cards/main.py
+++ 2017_a_pratice/week6/H - Patchouli Spell Cards/main.py
@@ -1,11 +1,8 @@
-fac = [1]
-for i in range(1, 101):
-    fac.append(fac[-1]*i)
-
-
-def c(a, b):
-    return fac[a]/fac[b]/fac[a-b]
-
+C = [[0 for i in range(105)] for i in range(105)]
+for i in range(0, 102):
+    C[i][0]=1
+    for j in range(1, i+1):
+        C[i][j] = C[i-1][j] + C[i-1][j-1]
 
 def gcd(a, b):
     if b == 0:
@@ -24,7 +21,7 @@ while True:
             for i in range(1, n+1):
                 for j in range(1, m+1):
                     for k in range(0, min(j+1, l)):
-                        dp[i][j] += dp[i-1][j-k]*c(m-(j-k), k)
+                        dp[i][j] += dp[i-1][j-k]*C[m-(j-k)][k]
             tot = n ** m
             use = 0
             for i in range(1, n+1):
