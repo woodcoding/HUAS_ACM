73a360612cdd4c9acba7b58734a7129aa2f0904a
woodcoding <woodcoding@gmail.com>
2017-07-17 09:09:06 +0800

    状态压缩dp

--- 2017_a_pratice/lmweek4/D - 男神的约会/main.cpp
+++ 2017_a_pratice/lmweek4/D - 男神的约会/main.cpp
@@ -3,54 +3,30 @@
 #include <cstring>
 using namespace std;
 #define memset(a,b) memset(a, b, sizeof(a))
+#define INF 0x3f3f3f3f
+const int inf=0x3f3f3f3f;
 const int maxn=12;
 const int n=10;
-struct Node{
-    int cnt, cost;
-    int v[maxn];
-    Node(){
-        cnt=cost=0;
-        memset(v,0);
-    }
-};
 
-int data[maxn][maxn];
-Node dp[maxn][maxn];
+int data[maxn][maxn], dp[maxn][maxn][1<<10];
 
 int main()
 {
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=10;j++){
             scanf("%d", &data[i][j]);
         }
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            int v=data[i][j];
-            dp[i][j-1].cost+=v;
-            if(dp[i][j-1].v[v]==0){
-                dp[i][j-1].cnt++;
-                dp[i][j-1].v[v]=1;
-            }
-            dp[i-1][j].cost+=v;
-            if(dp[i-1][j].v[v]==0){
-                dp[i-1][j].cnt++;
-                dp[i-1][j].v[v]=1;
-            }
-            if(dp[i][j-1].cnt==dp[i-1][j].cnt){
-                if(dp[i][j-1].cost<dp[i-1][j].cost){
-                    dp[i][j]=dp[i][j-1];
-                }
-                else dp[i][j]=dp[i-1][j];
-            }
-            else if(dp[i][j-1].cnt>dp[i-1][j].cnt){
-                dp[i][j]=dp[i][j-1];
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
+    memset(dp, INF);
+    dp[1][1][1<<data[1][1]]=data[1][1];
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=10;j++){
+            if(i==1&&j==1)continue;
+            for(int k=0;k<=1023;k++){
+                dp[i][j][k|1<<data[i][j]]=min(dp[i][j][k], min(dp[i][j-1][k], dp[i-1][j][k])+data[i][j]);
             }
         }
     }
-    printf("%d\n", dp[n][n].cost);
+    printf("%d\n", dp[10][10][1023]);
     return 0;
 }
